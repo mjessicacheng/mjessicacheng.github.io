6c08c9a0b40a039eee736fa67b3ff421169341ed
mjessicacheng <mjessicacheng@gmail.com>
2019-09-20 02:35:34 -0700

    revamp look entirely

--- nav.js
+++ nav.js
@@ -1,34 +1,29 @@
+function mouseon() {
+  document.getElementById('cube').style.WebkitTransform = "rotate3d(1,1,1,0deg)"
+  document.getElementById('cube').style.color = "rgba(0,0,0,1)";
+  document.getElementsByClassName('front')[0].style.color = "rgba(0,0,0,0)";
+  document.getElementsByClassName('front')[0].style.opacity = "0";
 
-function one() {
-    document.getElementsByClassName('spinning')[0].style.WebkitTransform = "translateZ( -100px )";
-    //document.getElementById("cube").removeAttribute("spinning");
-  }   
+  document.getElementsByClassName('top')[0].style.WebkitTransform = "rotateX(-180deg)"
+  document.getElementsByClassName('bottom')[0].style.WebkitTransform = "rotateX(180deg)";
+  document.getElementsByClassName('left')[0].style.WebkitTransform = "rotateY(0deg) translateX(-100px)";
+  document.getElementsByClassName('right')[0].style.WebkitTransform = "rotateY(0deg) translateX(100px)";
+  document.getElementsByClassName('front')[0].style.WebkitTransform = "translateZ(50px)";
+  document.getElementsByClassName('back')[0].style.WebkitTransform = "translateZ(0px)";
 
-  function two() {
-    document.getElementsByClassName('spinning')[0].style.WebkitTransform = "translateZ( -100px )  rotateX( -180deg )";
-    //document.getElementById("cube").removeAttribute("spinning");
-  }   
+}
 
-  function three() {
-    document.getElementsByClassName('spinning')[0].style.WebkitTransform = "translateZ( -100px ) rotateY(  -90deg )";
-    //document.getElementById("cube").removeAttribute("spinning");
-  }
+function mouseoff() {
+  document.getElementById('cube').style.WebkitTransform = "rotate3d(1,1,1,20deg)"
+  document.getElementById('cube').style.color = "rgba(0,0,0,0)";
+  document.getElementsByClassName('front')[0].style.color = "rgba(0,0,0,1)";
+  document.getElementsByClassName('front')[0].style.opacity = "1";
 
-  function four() {
-    document.getElementsByClassName('spinning')[0].style.WebkitTransform = "translateZ( -100px ) rotateY(   90deg )";
-    //document.getElementById("cube").removeAttribute("spinning");
-  }     
+  document.getElementsByClassName('top')[0].style.WebkitTransform = "rotateX(90deg) translateY(50px)";
+  document.getElementsByClassName('bottom')[0].style.WebkitTransform = "rotateX(-90deg) translateY(-50px) ";
+  document.getElementsByClassName('left')[0].style.WebkitTransform = "rotateY(-90deg) translateX(50px) translateZ(100px)";
+  document.getElementsByClassName('right')[0].style.WebkitTransform = "rotateY(90deg) translateX(-50px) translateZ(100px)";
+  document.getElementsByClassName('front')[0].style.WebkitTransform = "rotateY(0deg) translateZ(50px)";
+  document.getElementsByClassName('back')[0].style.WebkitTransform = "translateZ(-50px)";
+}
 
-  function five() {
-    document.getElementsByClassName('spinning')[0].style.WebkitTransform = "translateZ( -100px ) rotateX(  -90deg )";
-    //document.getElementById("cube").removeAttribute("spinning");
-  }   
-
-  function six() {
-    document.getElementsByClassName('spinning')[0].style.WebkitTransform = "translateZ( -100px ) rotateX(   90deg )";
-    //ocument.getElementById("cube").removeAttribute("spinning");
-  }   
-
-  function mouseoff() {
-    document.getElementsByClassName('spinning')[0].style.WebkitTransform = "translateZ( -100px )";
-  }
